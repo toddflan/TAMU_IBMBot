3e3f0a9bde9b2f877ae6288d293eb3e969771133
toddflan <toddflanagan95@gmail.com>
2018-03-24 02:25:55 -0500

    cleaned up SenComm/

--- SenComm/on_fall.sh
+++ SenComm/on_fall.sh
@@ -7,5 +7,5 @@ file="falls.txt"
 while true
 do
 	inotifywait -e modify $file
-	curl -X POST -d "I've detected a fall. Would you like me to get help?" -H "Content-type: text/plain" http://localhost:1880/reminder
+	curl -X POST -d "I've detected a fall. Would you like me to get help?" -H "Content-type: text/plain" http://tjSA:1880/reminder
 done
